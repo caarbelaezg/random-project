8cb53dcf4456de95655837316023e4a900848928
caarbelaezg <caarbelaezg@gmail.com>
2023-06-22 09:53:11 -0500

    Adding jest and footer to the login page

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css'
+export const Footer = () => {
+  return (
+    <footer className="footer">
+
+    </footer>
+  )
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,32 +4,54 @@ import "./styles.css";
 
 export const Login = () => {
   return (
-    <div className="loginPage">
-      <div className="loginPage__sectionA">
-        <div className="loginPage__description">
-          <h1>Untitled UI</h1>
-          <p>Just for fun</p>
+    <>
+      <div className="loginPage">
+        <div className="loginPage__sectionA">
+          <div className="loginPage__description">
+            <h1>Untitled UI</h1>
+            <p>Just for fun</p>
+          </div>
+          <motion.div
+            animate={{
+              scale: [1, 2, 2, 1, 1],
+              rotate: [0, 0, 180, 180, 0],
+              borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+              repeat: Infinity,
+              repeatDelay: 1,
+            }}
+            className="loginPage__circle"
+          ></motion.div>
+          <div className="loginPage__blur"></div>
         </div>
-        <motion.div
-          animate={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 180, 180, 0],
-            borderRadius: ["0%", "0%", "50%", "50%", "0%"],
-          }}
-          transition={{
-            duration: 2,
-            ease: "easeInOut",
-            times: [0, 0.2, 0.5, 0.8, 1],
-            repeat: Infinity,
-            repeatDelay: 1,
-          }}
-          className="loginPage__circle"
-        ></motion.div>
-        <div className="loginPage__blur"></div>
+        <div className="loginPage__sectionB">
+          <LoginForm />
+        </div>
+        <footer className="loginPage__footer">
+          <h5>© 2023 Untitled UI. All rights reserved.</h5>
+          <ul className="loginPage__footer-list">
+            <li className="loginPage__footer-list-item">
+              <a className="loginPage__footer-link" href="#">
+                Terms of use
+              </a>
+            </li>
+            <li className="loginPage__footer-list-item">
+              <a className="loginPage__footer-link" href="#">
+                Privacy
+              </a>
+            </li>
+            <li className="loginPage__footer-list-item">
+              <a className="loginPage__footer-link" href="#">
+                Cookies
+              </a>
+            </li>
+          </ul>
+        </footer>
       </div>
-      <div className="loginPage__sectionB">
-        <LoginForm />
-      </div>
-    </div>
+    </>
   );
 };
